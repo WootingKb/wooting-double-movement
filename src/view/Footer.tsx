9969f1c16638d0a585006d850a3e8ec93ad46c17
Jeroen <jeroen@wooting.nl>
2021-04-16 16:14:14 +0200

    Add ViGEm credit

--- src/view/Footer.tsx
+++ src/view/Footer.tsx
@@ -9,14 +9,19 @@ export function Footer() {
   ipcRenderer.invoke("getVersion").then(setVersion).catch(console.error);
 
   return (
-    <Flex p="6" pt="0">
+    <Flex p="6" pt="0" justifyContent="space-between">
       <Text variant="body" fontSize="sm">
         <Link href="https://wooting.io/double-movement" isExternal>
           More information
           <ExternalLinkIcon mx="2px" />
         </Link>
       </Text>
-      <Spacer />
+      <Text variant="body" fontSize="sm">
+        <Link href="https://github.com/ViGEm" isExternal>
+          Powered by ViGEm
+          <ExternalLinkIcon mx="2px" />
+        </Link>
+      </Text>
       <Text variant="body" fontSize="sm">
         Version: {version}
       </Text>
